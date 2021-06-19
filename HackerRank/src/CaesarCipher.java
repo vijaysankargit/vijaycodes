1125bc9cec82b0a95444e445b824ea69109577c8
Binary <Binary@DESKTOP-84D9QNI>
2021-06-19 19:37:39 +0530

    refs #caesarcipher program final working commit

--- HackerRank/src/CaesarCipher.java
+++ HackerRank/src/CaesarCipher.java
@@ -7,23 +7,19 @@ import java.io.InputStreamReader;
 public class CaesarCipher {
 
 	public static String caesarCipher(String s, int k) {
-	    char[] alphabets = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	    String alphabets = "^[a-zA-Z]*$";
 	    StringBuilder sb = new StringBuilder();
 	    for(int i = 0;i < s.length();i++){
-	        for(int j = 0;j < alphabets.length;j++){
-	            if (new String(alphabets).indexOf(s.charAt(i)) == -1) {
-	                sb.append(s.charAt(i));
-	                break;
+	            if (String.valueOf(s.charAt(i)).matches(alphabets)) {
+	            	int base = Character.isLowerCase(s.charAt(i)) ? 'a' : 'A';
+	            	char resultValue = (char)((s.charAt(i) + k - base) % 26 + base);
+	            	sb.append(resultValue);
 	            }else{
-	                int appendValue = new String(alphabets).indexOf(s.charAt(i));
-	                sb.append(alphabets[appendValue + k]);
-	                break;
-	            }
-	        }
+	                sb.append(s.charAt(i));
+	            }    
 	    }
 	    System.out.println("finalValue" + sb);
-	    return sb.toString();
-	    
+	    return sb.toString(); 
 	    }
 	
 	    public static void main(String[] args) throws IOException {
